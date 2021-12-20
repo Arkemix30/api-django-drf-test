fda1793ff1a62753c59f4830d902b031a3ef8776
Enmanuel Silva <silvaenmanuel@hotmail.com>
2021-12-19 22:01:59 -0600

    feat(auth): added authentication using django auth user model
    also added rest_framework urls for login and logout

---
+++ api/auth/views.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+from rest_framework import generics
+
+from .serializers import RegisterSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    """This view should register an user for authentication"""
+
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
